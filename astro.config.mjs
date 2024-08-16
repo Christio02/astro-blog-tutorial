d8a1c2df5c8d960f782f2c404cfdbf0c1deb99ed
Christopher Høe <christopher.hoee@gmail.com>
2024-08-16 12:31:34 +0200

    finished part 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
-import icon from "astro-icon";
+import icons from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [
+    icons({
+      collections: {
+        ic: '@iconify-json/ic',  // Ensure this is the correct path
+        // Add more collections if needed
+      },
+    }),
+  ],
 });
